89455d26a209ec13458cda58b325620f78afe5d1
Razwanul <razwanulghani@gmail.com>
2021-09-10 16:25:03 +0600

    ghani

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -91,7 +91,7 @@ class FrontEndController extends Controller
         $distance = Distance::where('from', $item->fromDistrict->name)
             ->where('to', $item->toDistrict->name)->first()->distance;
 
-            dd($distance);
+            dd($totalWeight);
 
             if($distance <= 100){
                 $ratio = .05;
